d67484e06d3eb3761b66af3a1286877e22c00f2c
ahmed unis <slemona234@gmail.com>
2026-01-02 14:45:59 +0300

    first model

---
+++ Test1/src/main/java/com/Test1/Test1/Age.java
@@ -0,0 +1,13 @@
+package com.Test1.Test1;
+
+public class Age {
+    private int day;
+    private int month;
+    private int year;
+    
+    public Age(int day,int month,int year){
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    }
+}
--- Test1/src/main/java/com/Test1/Test1/FirstController.java
+++ Test1/src/main/java/com/Test1/Test1/FirstController.java
@@ -1,8 +1,18 @@
 package com.Test1.Test1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class FirstController {
+    
+    @GetMapping("/Age")
+    public String getAge(Model model){
+        Age firstAge = new Age(1, 1, 2016);
+        model.addAttribute("Age",firstAge);
+        System.out.println("hi");
+        return "age";
+    }
     
 }
